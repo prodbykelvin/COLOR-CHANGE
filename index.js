e3f2b707b512aa282f212e94e6871f477cecb41d
prodbykelvin <152040169+prodbykelvin@users.noreply.github.com>
2023-12-01 13:30:05 +0100

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+var color = ["#ff3d3d", "blue", "green", "yellow", "purple"];
+
+var colorChange = document.getElementById("change-color");
+var button = document.getElementById("button");
+
+let counter = 0;
+let newcounter = 0;
+let isButtonDown = false;
+let clickCount = 0;
+var changeColor = function () {
+  //   if (counter >= 4) {
+  //     counter = 0;
+  //   }
+  counter = (counter + 1) % color.length;
+  colorChange.style.background = color[counter];
+};
+// for double click event
+var loopColor = function () {
+  if (newcounter >= 4) {
+    newcounter = 0;
+  }
+  //   newcounter = (newcounter + 1) % color.length;
+  colorChange.style.background = color[newcounter];
+  newcounter++;
+};
+// i was trying to attemt a double click event here but i got stuck along the line
+button.addEventListener("mousedown", function checkClick() {
+  if (clickCount === 2) {
+    // isButtonDown = true;
+    addEventListener("dbclick", setInterval(loopColor, 2000));
+  }
+});
+
+button.addEventListener("click", changeColor);
 
